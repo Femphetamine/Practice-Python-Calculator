2f71593fa84218b11d358a94bd12236582ad1f81
Femhpetamine <siltsu0@gmail.com>
2026-03-25 12:19:43 +0200

    update

--- main.py
+++ main.py
@@ -8,4 +8,17 @@ def startMenu():
     else:
         print("Error!")
         askToRestart()
+    #end
+#end
+
+def askToRestart():
+    print("Program complete.")
+    print("1. Restart program")
+    print("2. Exit program")
+    selection = int(input("Select : "))
+    if selection == 1:
+        startMenu()
+    elif selection == 2:
+        print("Bye bye!")
+    #end
 #end
